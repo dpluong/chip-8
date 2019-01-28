08c3eaf68db0679cadb3cdaf637c3cb995859e26
helloworld111gh <helloworld111gh@users.noreply.github.com>
2019-01-28 13:38:23 -0800

    start work

--- src/frontend/frontend.js
+++ src/frontend/frontend.js
@@ -1,5 +1,30 @@
 class Frontend {
-
+  constructor() {
+    this.currentDisplay = [];
+  }
+  
+  /** Gets the current buttons being pressed by the user 
+   * @returns 
+  */
+  getCurrentInputs() {
+    return {
+      1: false,
+      2: false,
+      3: false,
+      4: false,
+      5: false,
+      6: false,
+      7: false,
+      8: false,
+      9: false,
+      a: false,
+      b: false,
+      c: false,
+      d: false,
+      e: false,
+      f: false,
+    }
+  }
 }
 
 export default Frontend;
