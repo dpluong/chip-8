8a6d4ac175907a1db480527eadf193fd1fcf761c
helloworld111gh <helloworld111gh@users.noreply.github.com>
2019-02-04 13:25:45 -0800

    move backend to index js

--- src/index.js
+++ src/index.js
@@ -87,7 +87,7 @@ class Frontend {
       if (!this.codeInputDOM.files.length) return; // If no file then silently ignore
       const fileReader = new FileReader();
       fileReader.onloadend = () => {
-        callback(new Int8Array(fileReader.result)); // send in Int8Array as CHIP-8 is 8-bit program
+        callback(new Uint8Array(fileReader.result)); // send in Int8Array as CHIP-8 is 8-bit program
       };
       fileReader.readAsArrayBuffer(this.codeInputDOM.files[0]);
     });
@@ -97,3 +97,207 @@ class Frontend {
 
 const frontend = new Frontend();
 frontend.renderDisplay();
+
+// Chip 8 emulator
+
+
+// Chip 8 cpu
+
+class Chip8Cpu {
+  constructor() {
+    // Allocate memory
+    this.memory = new Uint8Array(4096);
+    // Allocate stack
+    this.stack = new Uint8Array(16);
+    // Set pointer to 0
+    this.pointer = 0;
+    // Set program counter to 0
+    this.pc = 0;
+    // Allocate 16 registers (V0->VF)
+    this.register = new Uint8Array(16);
+    // Set VI to 0
+    this.I = 0;
+    // Screen width
+    this.width = 64;
+    // Screen height
+    this.height = 32;
+    // Allocate memory for screen
+    this.screen = new Uint8Array(this.width * this.height);
+    // Set delay timer to 0
+    this.delay = 0;
+    // Set sound timer tp 0
+    this.sound = 0;
+    // Allocate memory for keyboard
+    this.keyboard = new Uint8Array(16);
+    // A flag to check if program is running
+    this.isRunning = false;
+    // A flag to check if draw opcode is called
+    this.drawFlag = false;
+    // Check if a key is pressed
+    this.isPressed = false;
+    // not completed ???
+    this.chip8_font = [
+      0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+      0x20, 0x60, 0x20, 0x20, 0x70, // 1
+      0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+      0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+      0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+      0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+      0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+      0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+      0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+      0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+      0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+      0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+      0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+      0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+      0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+      0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+    ];
+  }
+
+  loadProgram() {
+  }
+
+  drawSprite(x, y, i) {
+    let sprite;
+    this.register[16] = 0;
+
+    if (x > this.width) {
+      x -= this.width;
+    } else if (x < 0) {
+      x += this.width;
+    }
+
+    if (y > this.height) {
+      y -= this.height;
+    } else if (y < 0) {
+      x += this.height;
+    }
+
+    const index = x + y * this.width;
+
+    for (let y2 = 0; y < this.height; y2 += 1) {
+      sprite = this.memory(i + y2);
+      for (let x2 = 0; x2 < 8; x2 += 1) {
+        if ((sprite & (0x80 >> x)) != 0) {
+          if ((this.screen[index] ^= 1) == 0);
+          this.register[16] = 1;
+          this.screen[index] ^= 1;
+        }
+      }
+    }
+  }
+
+  runNextInstruction() {
+    const oc = this.memory[this.pc] << 8 | this.memory[this.pc + 1];
+    this.pc = this.pc + 2;
+
+    const x = oc & 0x0F00 >> 8;
+    const y = oc & 0x00F0 >> 4;
+    const nnn = oc & 0x0FFF;
+    const nn = oc & 0x00FF;
+    const n = oc & 0x000F;
+
+    switch (oc & 0xF000) {
+      case 0x00E0:
+      {
+        for (let i = 0; i < this.screen.length; i += 1) {
+          this.screen = 0;
+        }
+        break;
+      }
+
+      case 0x00EE:
+      {
+        this.pointer -= 1;
+        this.pc = this.stack[this.pointer];
+        break;
+      }
+
+      case 0x1000:
+      {
+        this.pc = nnn;
+        break;
+      }
+
+      case 0x2000:
+      {
+        this.pointer += 1;
+        this.stack.push(this.pc);
+        this.pc = nnn;
+
+        break;
+      }
+
+      case 0x3000:
+      {
+        if (this.register[x] == nn) {
+          this.pc += 2;
+        }
+        break;
+      }
+
+      case 0x4000:
+      {
+        if (this.register[x] != nn) {
+          this.pc += 2;
+        }
+        break;
+      }
+
+      case 0x5000:
+      {
+        if (this.register[x] == this.register[y]) {
+          this.pc += 2;
+        }
+        break;
+      }
+
+      case 0x6000:
+      {
+        this.register[x] = nn;
+        break;
+      }
+
+      case 0x7000:
+      {
+        this.register[x] += nn;
+        break;
+      }
+
+      case 0x8000:
+      {
+        break;
+      }
+
+      case 0x9000:
+      {
+        if (this.register[x] != this.register[y]) {
+          this.pc += 2;
+        }
+        break;
+      }
+
+      case 0xA000:
+      {
+        this.I = nnn;
+        break;
+      }
+
+      case 0xB000:
+      {
+        this.pc = nnn + this.register[0];
+        break;
+      }
+
+      default:
+      {
+        break;
+      }
+    }
+  }
+}
+
+const backend = new Chip8Cpu();
+frontend.statwaitForCodeInput(code => backend.loadProgram(code));
